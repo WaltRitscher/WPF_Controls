07c2b82426944d781fbd76ebdcad680682b10ed5
Walt Ritscher <waltritscher@outlook.com>
2017-04-21 13:30:51 -0700

    Add visibiltiy Demos

---
+++ WPF_Controls/Pages/HideAnimationPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControls.Pages
+{
+  /// <summary>
+  /// Interaction logic for HideAnimationPage.xaml
+  /// </summary>
+  public partial class HideAnimationPage : Page
+  {
+    public HideAnimationPage()
+    {
+      InitializeComponent();
+    }
+
+    private void Grid2_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+      FadeOut();
+    }
+
+    private void FadeOut()
+    {
+      var animation = new DoubleAnimation
+      {
+        To = 0,
+        Duration = TimeSpan.FromSeconds(2),
+        FillBehavior = FillBehavior.Stop
+      };
+
+
+      animation.Completed += (s, a) =>
+      {
+        Grid2.Opacity = 0;
+        Grid2.Visibility = Visibility.Collapsed;
+      };
+
+      Grid2.BeginAnimation(UIElement.OpacityProperty, animation);
+
+
+    }
+  }
+}
---
+++ WPF_Controls/Pages/HitTestPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControls.Pages
+{
+  /// <summary>
+  /// Interaction logic for HitTestPage.xaml
+  /// </summary>
+  public partial class HitTestPage : Page
+  {
+    public HitTestPage()
+    {
+      InitializeComponent();
+    }
+
+    private void Grid2_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+      Message1.Text = Message1.Text + ".";
+    }
+
+    private void Grid5_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+      Message2.Text = Message2.Text + ".";
+    }
+  }
+}
