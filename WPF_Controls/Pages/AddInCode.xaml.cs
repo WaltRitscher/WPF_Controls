55cbdf8f9c712b1f8078cfd836bd853d1082b8ee
Staff Author <staffauthor@outlook.com>
2017-04-20 21:12:57 -0700

    Add comments to the AddInCode page

--- WPF_Controls/Pages/AddInCode.xaml.cs
+++ WPF_Controls/Pages/AddInCode.xaml.cs
@@ -13,16 +13,17 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfControls.Pages
-{
-  /// <summary>
-  /// Interaction logic for AddInCode.xaml
-  /// </summary>
-  public partial class AddInCode : Page
-  {
-    public AddInCode()
-    {
-      InitializeComponent();
-    }
-  }
+namespace WpfControls.Pages {
+	/// <summary>
+	/// Interaction logic for AddInCode.xaml
+	/// </summary>
+	public partial class AddInCode : Page {
+		public AddInCode() {
+			InitializeComponent();
+
+			// Don't attempt to access the elements before the call
+			// to InitializeComponent.  The elements have not been properly
+			// initialized yet.
+		}
+	}
 }
