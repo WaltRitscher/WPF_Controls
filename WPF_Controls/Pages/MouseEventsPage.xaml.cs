7db9a4bbf9f848ad3c7abb59dd57e2d52129b914
Walt Ritscher <waltritscher@outlook.com>
2017-04-21 14:40:45 -0700

    Add mouse Event Demos

---
+++ WPF_Controls/Pages/MouseEventsPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControls.Pages
+{
+  /// <summary>
+  /// Interaction logic for MouseEventsPage.xaml
+  /// </summary>
+  public partial class MouseEventsPage : Page
+  {
+    public MouseEventsPage()
+    {
+      InitializeComponent();
+    }
+
+
+
+    private void GridA_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      // fires when any mouse button is down.
+      MessageTextBlock.Text = "GridA Button Down.";
+    }
+    private void GridA_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+      MessageTextBlock.Text = "GridA Button Up.";
+
+    }
+    private void GridB_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+      // examine the MouseButtonEventArgs args
+      string message = $"Mouse Device: {e.MouseDevice.ToString()}\nMouse Button: {e.ChangedButton}";
+      MessageTextBlock.Text = message;
+    }
+    private void GridC_MouseMove(object sender, MouseEventArgs e)
+    {
+      string positionMessage = $"X: {e.GetPosition(this).X.ToString("n0")}\nY: {e.GetPosition(this).Y.ToString("n0")}";
+      MessageTextBlock.Text = positionMessage;
+    }
+    private void GridD_MouseMove(object sender, MouseEventArgs e)
+    {
+      // get the button state during move
+      string positionMessage = $"X: {e.GetPosition(this).X.ToString("n0")}\nY: {e.GetPosition(this).Y.ToString("n0")}\n";
+      string buttonStateMessage = $"LeftButton: {e.LeftButton}\nMiddleButton: {e.MiddleButton}\nMiddleButton: {e.RightButton}";
+      MessageTextBlock.Text = positionMessage + buttonStateMessage;
+    }
+
+
+
+    private void GridE_MouseEnter(object sender, MouseEventArgs e)
+    {
+      Ellipse1.Fill = Brushes.Orange;
+    }
+
+    private void GridE_MouseLeave(object sender, MouseEventArgs e)
+    {
+      Ellipse1.Fill = Brushes.LightCoral;
+    }
+
+    private void GridF_MouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      if (e.Delta > 0)
+      {
+        // positive direction
+        GridScaleTransform.ScaleX = GridScaleTransform.ScaleY = GridScaleTransform.ScaleX + .01;
+      }
+      else
+      {
+        GridScaleTransform.ScaleX = GridScaleTransform.ScaleY = GridScaleTransform.ScaleX - .01;
+
+      }
+
+    }
+  }
+}
