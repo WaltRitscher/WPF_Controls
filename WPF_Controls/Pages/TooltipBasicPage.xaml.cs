188dcc980de5371fff6af675ff8acb0455d31fd6
Walt Ritscher <waltritscher@outlook.com>
2017-04-23 17:46:31 -0700

    Add tooltip demos

---
+++ WPF_Controls/Pages/TooltipBasicPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace WpfControls.Pages
+{
+  public partial class TooltipBasicPage : Page
+  {
+    public TooltipBasicPage()
+    {
+      InitializeComponent();
+    }
+
+    private void Image_ToolTipOpening(object sender, ToolTipEventArgs e)
+    {
+      var currentImage = sender as Image;
+      if (currentImage!= null)
+      {
+        currentImage.ToolTip = $"This tooltip created at {DateTime.Now.ToLongTimeString()}";
+      }
+    }
+  }
+}
