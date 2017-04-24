c265ff341517a13dfa7caf31a92c69e15df1436a
Walt Ritscher <waltritscher@outlook.com>
2017-04-23 21:33:32 -0700

    Add sizes demo

--- WPF_Controls/Pages/SizesPage.xaml.cs
+++ WPF_Controls/Pages/SizesPage.xaml.cs
@@ -22,7 +22,19 @@ namespace WpfControls.Pages
   {
     public SizesPage()
     {
-      InitializeComponent(); this.DesiredSize
+      InitializeComponent(); 
+    }
+
+    private void LeftActionButton_Click(object sender, RoutedEventArgs e)
+    {
+      string message = $"Width: {LeftActionButton.Width}\nDesired Width: {LeftActionButton.DesiredSize.Width}\nActualSize: {LeftActionButton.ActualWidth}";
+      LeftMessage.Text = message;
+    }
+
+    private void RightActionButton_Click(object sender, RoutedEventArgs e)
+    {
+      string message = $"Width: {RightActionButton.Width}\nDesired Width: {RightActionButton.DesiredSize.Width}\nActualSize: {RightActionButton.ActualWidth}";
+      RightMessage.Text = message;
     }
   }
 }
