86052641702f3876196e3bf3eee3c05114fda6e1
Walt Ritscher <waltritscher@outlook.com>
2017-04-26 21:07:32 -0700

    Add DrawPathPage, add demo to PathPage

--- WPF_Controls/Pages/PathPage.xaml.cs
+++ WPF_Controls/Pages/PathPage.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfControls.Pages
@@ -23,6 +14,76 @@ namespace WpfControls.Pages
     public PathPage()
     {
       InitializeComponent();
+
+      foreach (CheckBox item in MainStackPanel.Children.OfType<CheckBox>())
+      {
+        item.IsChecked = true;
+      }
+      PathDataTextBlock.Text = String.Empty;
+    }
+
+    private void CircleBackPath_Checked(object sender, RoutedEventArgs e)
+    {
+      CircleBackPath.Opacity = 1;
+      ShowPathData(CircleBackPath);
+    }
+
+    private void CircleBackPath_Unchecked(object sender, RoutedEventArgs e)
+    {
+      CircleBackPath.Opacity = 0;
+      ShowPathData(CircleBackPath);
+    }
+
+    private void GradientPathButton_Checked(object sender, RoutedEventArgs e)
+    {
+      Path3.Opacity = 1;
+      ShowPathData(Path3);
+    }
+
+    private void GradientPathButton_Unchecked(object sender, RoutedEventArgs e)
+    {
+      Path3.Opacity = 0;
+      ShowPathData(Path3);
+    }
+
+    private void Path4Button_Checked(object sender, RoutedEventArgs e)
+    {
+      CheckPath.Opacity = 1;
+      ShowPathData(CheckPath);
+    }
+
+    private void Path4Button_Unchecked(object sender, RoutedEventArgs e)
+    {
+      CheckPath.Opacity = 0;
+      ShowPathData(CheckPath);
+    }
+
+    private void Path5Button_Checked(object sender, RoutedEventArgs e)
+    {
+      CheckOutlinePath.Opacity = 1;
+      ShowPathData(CheckOutlinePath);
+    }
+
+    private void Path5Button_Unchecked(object sender, RoutedEventArgs e)
+    {
+      CheckOutlinePath.Opacity = 0;
+      ShowPathData(CheckOutlinePath);
+    }
+
+    private void ShowPathData(Path showPath)
+    {
+      PathDataTextBlock.Text = FormatGeometryDataString(showPath.Data.ToString());
+    }
+
+    private string FormatGeometryDataString(string dataString)
+    {
+      dataString = dataString.Replace("F", "FillRule: ");
+      dataString = dataString.Replace("M", "\nStart Point (M): ");
+
+      dataString = dataString.Replace("C", "\nBezier (C): ");
+      dataString = dataString.Replace("L", "\nLine (L): ");
+      dataString = dataString.Replace("Q", "\nQuadratic Bezier (C): ");
+      return dataString;
     }
   }
 }
