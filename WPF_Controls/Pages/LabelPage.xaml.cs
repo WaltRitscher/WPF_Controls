2eaa2da979e8494dce29798a4fe0d692dd1d35af
Walt Ritscher <waltritscher@outlook.com>
2017-04-30 19:12:29 -0700

    Add class diagrams and demo pages

---
+++ WPF_Controls/Pages/LabelPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControls.Pages
+{
+  /// <summary>
+  /// Interaction logic for LabelPage.xaml
+  /// </summary>
+  public partial class LabelPage : Page
+  {
+    public LabelPage()
+    {
+      InitializeComponent();
+    }
+    private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+      MessageBox.Show("Button clicked");
+    }
+  }
+}
