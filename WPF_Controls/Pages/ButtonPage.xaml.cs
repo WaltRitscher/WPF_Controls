40fac1278cd75e46dcc0c3f88fb345c0e815d432
Walt Ritscher <waltritscher@outlook.com>
2017-05-02 18:45:41 -0700

    Add travel logo image Add button content and event handlers

--- WPF_Controls/Pages/ButtonPage.xaml.cs
+++ WPF_Controls/Pages/ButtonPage.xaml.cs
@@ -24,5 +24,50 @@ namespace WpfControls.Pages
     {
       InitializeComponent();
     }
+
+    private void OpenButton_Click(object sender, RoutedEventArgs e)
+    {
+      // RoutedEventArgs 
+
+      ClearMessages();
+    }
+
+    private void SaveButton_Click(object sender, RoutedEventArgs e)
+    {
+      ClearMessages();
+      ClickEventMessageTextBlock.Text = $"Save Button, Click {DateTime.Now.Second}";
+    }
+
+    private void All_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      ClearMessages();
+      MouseMessageTextBlock.Text = $"Mouse Up {DateTime.Now.Second}";
+    }
+
+    private void All_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+      ClearMessages();
+      MouseMessageTextBlock.Text = $"Mouse Up {DateTime.Now.Second}";
+    }
+
+    private void ClearMessages()
+    {
+      ClickEventMessageTextBlock.Text = string.Empty;
+      MouseMessageTextBlock.Text = String.Empty;
+      TouchEventMessageTextBlock.Text = string.Empty;
+      
+    }
+
+    private void All_TouchDown(object sender, TouchEventArgs e)
+    {
+      ClearMessages();
+      TouchEventMessageTextBlock.Text = $"Touch Down {DateTime.Now.Second}";
+    }
+
+    private void All_TouchUp(object sender, TouchEventArgs e)
+    {
+      ClearMessages();
+      TouchEventMessageTextBlock.Text = $"Touch Up {DateTime.Now.Second}";
+    }
   }
 }
