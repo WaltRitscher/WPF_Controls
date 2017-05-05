0da3e2a385eb87d56f1e5033829b0e6fa5e57a37
Staff Author <staffauthor@outlook.com>
2017-05-05 00:54:38 -0700

    Add Calendar Controls, update progress bar demos

--- WPF_Controls/Pages/ProgressBarPage.xaml.cs
+++ WPF_Controls/Pages/ProgressBarPage.xaml.cs
@@ -15,14 +15,39 @@ using System.Windows.Shapes;
 
 namespace WpfControls.Pages
 {
-  /// <summary>
-  /// Interaction logic for ProgressBarPage.xaml
-  /// </summary>
-  public partial class ProgressBarPage : Page
-  {
-    public ProgressBarPage()
-    {
-      InitializeComponent();
-    }
-  }
+	/// <summary>
+	/// Interaction logic for ProgressBarPage.xaml
+	/// </summary>
+	public partial class ProgressBarPage : Page
+	{
+		public ProgressBarPage()
+		{
+			InitializeComponent();
+		}
+
+		private void CheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			Progress1.IsIndeterminate = true;
+		}
+
+		private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			Progress1.IsIndeterminate = false;
+		}
+		private Random _random = new Random();
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			// set the maximum value to the number of steps used in the process
+
+			Progress2.Maximum = 42; // number of files to download
+
+			// do the work, update the progress value
+			for (int counter = 0; counter < 43; counter++)
+			{
+				Progress2.Value = counter;
+				await Task.Delay(_random.Next(50, 300));
+
+			}
+		}
+	}
 }
